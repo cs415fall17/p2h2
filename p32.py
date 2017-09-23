d32c48e89838c8a96ab4eb75a8bf0ff666636a84
David Tauraso <david318@me.com>
2017-09-22 18:45:37 -0700

    added changes

--- p32.py
+++ p32.py
@@ -1,28 +1,33 @@
 import math
 
-def compute(l, N):
+def compute(N):
 
-    # Check whether the square root falls between intervals                  
-    while( l <= N ):
-        mid = math.floor(l+N/2)
+    l = 1
+    u = N
+    # Check whether the square root falls between intervals      
+    limit = 0            
+    while( l <= u ):
+        if limit == 10:
+            break
+        mid = math.floor(l+u/2)
+        print(mid)
         # Check to see if the given numbre is a square of middle             
-        if( mid*mid == N ):
-            print(N)
-            return 0
+        if( (mid*mid) == N ):
+            return N
         # If N is not a square, increment value of l                         
-        elif( N > mid*mid ):
+        elif( N > (mid*mid) ):
             l =mid + 1
             # If N is not a square, increment value of u                     
-        elif( N < mid*mid ):
+        elif( N < (mid*mid) ):
             # Assign value of u                                              
             u =mid - 1
         else:
             # N is not a square                                              
             print ("Sorry dude, N is not a square.")
             break
+        limit = limit + 1
 def main():
-    l =	int(input("Enter value for l: "))
     N =	int(input("Enter a value for N: "))
-    compute(l, N)
+    print(compute(N))
 
 main()
