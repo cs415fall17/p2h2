7194e0871b74d9df057277702af725d55d5d2c17
David Tauraso <david318@me.com>
2017-09-22 19:54:58 -0700

    Added powerOfNumberRecursive

--- p32.py
+++ p32.py
@@ -1,31 +1,72 @@
-import math
 
-def compute(N):
+def computePartA(N):
 
     l = 1
+
+    if N < 0: N = N * -1
+
     u = N
     # Check whether the square root falls between intervals      
-    limit = 0            
+
     while( l <= u ):
-        if limit == 10:
-            break
-        mid = math.floor(l+u/2)
-        print(mid)
-        # Check to see if the given numbre is a square of middle             
+
+        mid = (l+u) // 2
+
+        # Check to see if the given number is a square of middle             
         if( (mid*mid) == N ):
-            return N
+            return str(mid)
         # If N is not a square, increment value of l                         
         elif( N > (mid*mid) ):
             l =mid + 1
+
             # If N is not a square, increment value of u                     
         elif( N < (mid*mid) ):
             # Assign value of u                                              
             u =mid - 1
-        else:
-            # N is not a square                                              
-            print ("Sorry dude, N is not a square.")
-            break
-        limit = limit + 1
+
+    # N is not a square                                              
+    return "Sorry dude, N is not a square."
+
+def powerOfNumberRecursive(x, y):
+    # is x = y^n
+    # assume x goes into y evenly
+    # passed in first power of x
+    # base case because (y^n)/nx = 1 if x = y^n
+    # if y = 1 than there will be no power that can be added to 1 to change x from 1
+    if y == 1:
+        return 0
+    # if y > 1 than an exponent will exist such that x > 1 as long as y is divided by x
+    else:
+        return 1 + powerOfNumberRecursive(x, y // x)
+
+def computePartC(N):
+
+    l = 1
+
+    if N < 0: N = N * -1
+
+    u = N
+    # Check whether the square root falls between intervals      
+
+    while( l <= u ):
+
+        mid = (l+u) // 2
+
+        # Check to see if the given number is a square of middle             
+        if( (mid*mid) == N ):
+            return str(mid)
+        # If N is not a square, increment value of l                         
+        elif( N > (mid*mid) ):
+            l =mid + 1
+
+            # If N is not a square, increment value of u                     
+        elif( N < (mid*mid) ):
+            # Assign value of u                                              
+            u =mid - 1
+
+    # N is not a square                                              
+    return "Sorry dude, N is not a power."
+
 def main():
     N =	int(input("Enter a value for N: "))
     print(compute(N))
